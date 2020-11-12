fd499b17a637dfb5768bac779b9d29dd036c955f
Johan Wulf <johanwulf98@gmail.com>
2020-11-12 20:14:32 +0100

    added parcable class question

--- app/src/main/java/com/example/quizapp/Question.java
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -1,6 +1,9 @@
 package com.example.quizapp;
 
-public class Question {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Question implements Parcelable {
     String question;
     boolean correctAnswer;
 
@@ -16,4 +19,33 @@ public class Question {
     public boolean checkAnswer(boolean userInput) {
         return userInput == correctAnswer;
     }
+
+    protected Question(Parcel in) {
+        question = in.readString();
+        correctAnswer = in.readByte() != 0x00;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(question);
+        dest.writeByte((byte) (correctAnswer ? 0x01 : 0x00));
+    }
+
+    @SuppressWarnings("unused")
+    public static final Parcelable.Creator<Question> CREATOR = new Parcelable.Creator<Question>() {
+        @Override
+        public Question createFromParcel(Parcel in) {
+            return new Question(in);
+        }
+
+        @Override
+        public Question[] newArray(int size) {
+            return new Question[size];
+        }
+    };
 }
